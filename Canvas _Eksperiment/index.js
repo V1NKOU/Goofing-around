e5c50e545204c0902ab24422e0b1f88b743c68af
V1NKOU <victorkuzimski@gmail.com>
2026-06-23 04:20:55 +0200

    bouncy balls and colors!

--- Canvas _Eksperiment/index.js
+++ Canvas _Eksperiment/index.js
@@ -3,46 +3,71 @@ const ctx = canvas.getContext("2d")
 var mouseX
 var mouseY
 var balls = []
-canvas.addEventListener("mouseenter", locateMouse)
-
-function locateMouse() {
-    canvas.addEventListener("mousemove", trackMouse)
-    canvas.addEventListener("mouseleave", () => {
-        canvas.removeEventListener("mousemove", trackMouse)
-    })
-}
-function trackMouse(e) {
+var gravity = 0.6
+var selectedColor = {fill: "#c73d28", border: "#802f22"}
+canvas.addEventListener("mousemove", e => {
     mouseX = e.offsetX
     mouseY = e.offsetY
-}
-canvas.addEventListener("click", () => placeDot(mouseX, mouseY))
+})
 
-function placeDot(mouseX, mouseY) {
-    const ball = {
-        x: mouseX,
-        y: mouseY,
-        color: "black",
+canvas.addEventListener("click", () => createBall(mouseX, mouseY))
+
+document.querySelectorAll(".colorOption").forEach(e => {
+    e.addEventListener("click", () => {
+        document.querySelector(".colorOption.selected").classList.remove("selected")
+        e.classList.add("selected")
+        selectedColor = {fill: e.dataset.color, border: e.dataset.border}
+        console.log(selectedColor)
+    })
+    e.style.backgroundColor = e.dataset.color
+    e.style.borderColor = e.dataset.border
+})
+
+
+
+//START THE DRAWING STUFF!!
+
+loop()
+
+function createBall(x,y,) {
+    var ball = {
+        x: x,
+        y: y,
+        fillCol: selectedColor.fill,
+        borCol: selectedColor.border,
         radius: 20,
-        velY: 0,
+        velY: 1,
         velX: 0
     }
+    balls.push(ball)
+    placeBall(ball)
+}
+
+function placeBall(ball) {
     ctx.beginPath()
-    ctx.fillStyle = "black"
-    ctx.arc(mouseX,mouseY,20,0,2*Math.PI)
+    ctx.fillStyle = ball.fillCol
+    ctx.arc(ball.x,ball.y,ball.radius,0,2*Math.PI)
     ctx.fill()
-    ctx.strokeStyle = "red"
+    ctx.lineWidth = 3
+    ctx.strokeStyle = ball.borCol
     ctx.stroke()
     ctx.closePath()
-    balls.push(ball)
-    console.log(balls)
 }
 
 function loop() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
 
+    //
     for (let i = 0; i < balls.length; i++) {
-        
-        balls[i].ballX
+        //BALL POSITION UPDATE
+        balls[i].velY += gravity
+        balls[i].y += balls[i].velY
+        //BOUNCE!
+        if (balls[i].y + balls[i].radius >= canvas.height) {
+            balls[i].y = canvas.height - balls[i].radius
+            balls[i].velY = -(balls[i].velY-0.5)*0.9
+        }
+        placeBall(balls[i])
     }
     requestAnimationFrame(loop)
 }
